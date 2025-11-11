08992ad6e98a78af6603a6fd1bd03601336c1f48
yeongmin <hwongyoungmin@gmail.com>
2025-11-11 14:25:26 +0900

    feat: 게시글 목록 조회 화면 구현

--- public/assets/js/features/post/ui/postCard.js
+++ public/assets/js/features/post/ui/postCard.js
@@ -6,29 +6,29 @@ const POST_CARD_CSS_PATH = '/post/ui/postCard.css';
 
 activeFeatureCss(POST_CARD_CSS_PATH);
 
-export function postCard() {
-    console.log('postCard render');
-    // const { title, like, comment, viewCount, createdAt } = post;
-    // const { profile, nickname } = author;
+export function postCard(post) {
+    console.log('postCard render', post);
+    const { id, title, like, commentCount, hit, createdAt, authorNickname } = post;
 
     const root = document.createElement("div");
     root.className = 'post-card-container';
+    root.id = `post-card-${id}`;
     root.innerHTML =
         `
         <div class="post-card-wrapper">
             <button class="post-card-detail-btn">
                 <div class="post-card-summary-field">
-                    <h2 class="post-card-summary-title">제목 1</h2>
+                    <h2 class="post-card-summary-title">${title}</h2>
                     <div class="post-card-summary-info">
-                        <label class="post-card-summary-like">좋아요 0</label>
-                        <label class="post-card-summary-comment">댓글 0</label>
-                        <label class="post-card-summary-viewcount">조회 수 0</label>
-                        <label class="post-card-summary-createdat">2021-01-01 00:00:00</label>
+                        <label class="post-card-summary-like">좋아요 ${like}</label>
+                        <label class="post-card-summary-comment">댓글 ${commentCount}</label>
+                        <label class="post-card-summary-viewcount">조회 수 ${hit}</label>
+                        <label class="post-card-summary-createdat">${createdAt}</label>
                     </div>
                 </div>
                 <div class="post-card-author-field">
                     <div class="post-card-author-profile"></div>
-                    <div class="post-card-author-nickname">닉네임</div>
+                    <div class="post-card-author-nickname">${authorNickname}</div>
                 </div>
             </button>
         </div>
--- public/assets/js/features/post/ui/postCardList.js
+++ public/assets/js/features/post/ui/postCardList.js
@@ -1,19 +1,102 @@
 import { activeFeatureCss } from "../../../shared/lib/dom.js";
+import { Api } from "../../../shared/lib/api.js";
+import { ApiError } from "../../../shared/lib/api-error.js";
 import { postCard } from "./postCard.js";
 
 
 // CSS Path
 const POST_CARD_LIST_CSS_PATH = '/post/ui/postCardList.css';
 
+// API 요청 URL
+const POST_CARD_LIST_API_URL = '/post';
+
 activeFeatureCss(POST_CARD_LIST_CSS_PATH);
 
 export function postCardList() {
+    // 현재 페이지
+    let currentPage = 0;
+    // 페이지당 컨텐츠 개수
+    let size = 10;
+    // 다음 페이지 여뷰
+    let hasNext = true;
+    // 페이지 로딩 플래그
+    let isLoading = false;
+
+
     const root = document.createElement('div');
     root.className = 'post-card-list-container'
-    root.appendChild(postCard());
-    root.appendChild(postCard());
-    root.appendChild(postCard());
+
+    // 무한 스크롤용 sentinel 박스 생성
+    const sentinel = document.createElement('div');
+    sentinel.className = 'post-card-list-sentinel';
+    root.appendChild(sentinel);
+
+    addObserver();
+    loadNextPage();
 
 
     return root;
+
+
+    // 무한 스크롤용 옵저버 추가
+    function addObserver() {
+        const observer = new IntersectionObserver(async (entries) => {
+            const entry = entries[0];
+            if (entry.isIntersecting && !isLoading && hasNext) {
+                await loadNextPage();
+            }
+        }, { threshold: 0.5 });
+
+        observer.observe(sentinel);
+    }
+
+    // 무한 스크롤 시 페이지 로딩 함수
+    async function loadNextPage() {
+        try {
+
+            // 로딩 중이거나 다음 페이지 없으면 페이지 로드 X
+            if (isLoading || !hasNext) {
+                return;
+            }
+
+            // 페이지 로드 시작
+            isLoading = true;
+
+            const response = await requestPostCardList(currentPage, size);
+            const responseBody = response.data;
+
+            // Post 렌더링
+            const contents = responseBody.contents;
+            contents.forEach((post) => {
+                root.insertBefore(postCard(post), sentinel);
+            })
+
+            // 다음 로드 페이지 계산
+            currentPage = responseBody.currentPage + 1;
+            // 다음 페이지 여부
+            hasNext = responseBody.hasNext;
+        } catch (error) {
+            if (error instanceof ApiError) {
+                // TODO : API 에러 처리
+            }
+        } finally {
+            isLoading = false;
+        }
+    }
+
+    // 게시글 목록 조회 API 요청
+    // start, default Page = 0
+    async function requestPostCardList(page, size) {
+        try {
+            const response = await new Api()
+                .get()
+                .url(POST_CARD_LIST_API_URL)
+                .queryString({ page, size })
+                .request();
+
+            return response;
+        } finally {
+
+        }
+    }
 }
